eaa86d58d70f4e9655b2b82637e7241dd3e5f85f
Keeghan McGarry <keeghan_m@live.com>
2025-04-04 12:58:20 +0100

    Deleting lists

--- src/components/app/components/viewer/list.tsx
+++ src/components/app/components/viewer/list.tsx
@@ -1,34 +1,61 @@
+import { Button } from '@/components/ui/button'
 import {
   useListsStore,
   type ListWithSpots,
 } from '../../stores/lists'
 import Spot from './spot'
+import { useListsQueries } from '../../hooks/useListsQueries'
+import { useState } from 'react'
 
 interface ListProps {
   list: ListWithSpots
 }
 export default function List({ list }: ListProps) {
   const { currentList, selectList } = useListsStore()
+  const { deleteListMutation } = useListsQueries()
+  const [confirmDelete, setConfirmDelete] = useState(false)
+
+  const handleDeleteSpot = (e: React.MouseEvent) => {
+    e.stopPropagation()
+    if (confirmDelete) {
+      deleteListMutation.mutate(list.id)
+      setConfirmDelete(false)
+    } else {
+      setConfirmDelete(true)
+    }
+  }
 
   return (
     <div className="mx-auto w-lg max-w-full">
       <div
-        className={`cursor-pointer rounded border p-4 hover:bg-blue-100 ${currentList?.id === list.id ? 'bg-blue-100' : ''}`}
+        className={`flex cursor-pointer justify-between rounded border p-4 hover:bg-blue-100 ${currentList?.id === list.id ? 'bg-blue-100' : ''}`}
         onClick={() => {
+          setConfirmDelete(false)
           selectList(
             currentList?.id === list.id ? null : list,
           )
         }}
       >
-        <h2 className="text-xl font-bold">{list.name}</h2>
-        <p>
-          Visited:{' '}
-          {list.spots?.reduce(
-            (acc, spot) => acc + (spot.visited ? 1 : 0),
-            0,
-          )}
-          /{list.spots?.length ?? 0}{' '}
-        </p>
+        <div className="">
+          <h2 className="text-xl font-bold">{list.name}</h2>
+          <p>
+            Visited:{' '}
+            {list.spots?.reduce(
+              (acc, spot) => acc + (spot.visited ? 1 : 0),
+              0,
+            )}
+            /{list.spots?.length ?? 0}{' '}
+          </p>
+        </div>
+        <div className="">
+          <Button
+            variant="destructive"
+            disabled={deleteListMutation.isPending}
+            onClick={handleDeleteSpot}
+          >
+            {confirmDelete ? 'Are you sure?' : 'Delete'}
+          </Button>
+        </div>
       </div>
       {currentList?.id === list.id &&
         (list.spots?.length > 0 ? (
