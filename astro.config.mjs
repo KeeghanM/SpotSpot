9df214c0efd59af599043a7a32dea3dafc9b0f58
Keeghan McGarry <keeghan_m@live.com>
2025-06-01 23:05:24 +0100

    static site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import node from '@astrojs/node'
 import partytown from '@astrojs/partytown'
 import react from '@astrojs/react'
 import tailwindcss from '@tailwindcss/vite'
+import compress from 'astro-compress'
 import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), partytown(), icon()],
-  output: 'server',
+  integrations: [react(), partytown(), icon(), compress()],
+  output: 'static',
   adapter: node({ mode: 'standalone' }),
   vite: { plugins: [tailwindcss()] },
 })
